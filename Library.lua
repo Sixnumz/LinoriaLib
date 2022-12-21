833fee633169f975ab511b5eabbda06a2de15813
Sixnumz <115585031+Sixnumz@users.noreply.github.com>
2022-12-21 21:37:14 +0700

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -26,8 +26,8 @@ local Library = {
 
     HudRegistry = {};
 
-    FontColor = Color3.fromRGB(255, 255, 255);
-    MainColor = Color3.fromRGB(28, 28, 28);
+    FontColor = Color3.fromRGB(0, 255, 255);
+    MainColor = Color3.fromRGB(0, 255, 255);
     BackgroundColor = Color3.fromRGB(20, 20, 20);
     AccentColor = Color3.fromRGB(0, 85, 255);
     OutlineColor = Color3.fromRGB(50, 50, 50);
